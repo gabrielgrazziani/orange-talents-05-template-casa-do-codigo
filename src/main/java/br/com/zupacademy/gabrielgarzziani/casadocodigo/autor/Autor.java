a033e04d5dac320a40fe2adc8b78812edb63c18f
gabriel_grazziani <gabriel.grazzianibarbosa@gmail.com>
2021-05-17 13:41:22 -0300

    criando autor

---
+++ src/main/java/br/com/zupacademy/gabrielgarzziani/casadocodigo/autor/Autor.java
@@ -0,0 +1,60 @@
+package br.com.zupacademy.gabrielgarzziani.casadocodigo.autor;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.NotNull;
+
+@Entity
+public class Autor {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotBlank
+	private String nome;
+	@NotBlank
+	@Email
+	private String email;
+	@Size(max = 400)
+	private String descricao;
+	@NotNull
+	@PastOrPresent
+	private LocalDateTime instanteCriacao;
+
+	@Deprecated
+	public Autor() {
+	}
+		
+	public Autor(@NotBlank String nome, @NotBlank @Email String email, @Size(max = 400) String descricao,
+			@PastOrPresent LocalDateTime instanteCriacao) {
+		this.nome = nome;
+		this.email = email;
+		this.descricao = descricao;
+		this.instanteCriacao = instanteCriacao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public LocalDateTime getInstanteCriacao() {
+		return instanteCriacao;
+	}
+	
+}
