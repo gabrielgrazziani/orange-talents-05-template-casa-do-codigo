a2b8b9c39cc8c65398cf1ba4ffedb7ea84787f6f
gabriel_grazziani <gabriel.grazzianibarbosa@gmail.com>
2021-05-17 14:04:50 -0300

    criando AutorController

---
+++ src/main/java/br/com/zupacademy/gabrielgarzziani/casadocodigo/autor/AutorController.java
@@ -0,0 +1,24 @@
+package br.com.zupacademy.gabrielgarzziani.casadocodigo.autor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("autor")
+public class AutorController {
+	
+	@Autowired
+	private AutorRepository autorRepository;
+
+	@PostMapping
+	public ResponseEntity<AutorDto> cadastro(@RequestBody AutorForm form) {
+		Autor autor = form.map();
+		autorRepository.save(autor);
+		return ResponseEntity.ok(new AutorDto(autor));
+	}
+	
+}
