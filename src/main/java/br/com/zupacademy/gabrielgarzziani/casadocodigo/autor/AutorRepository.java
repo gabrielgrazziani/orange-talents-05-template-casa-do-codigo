ad4868ac7577a5c349e0809c3fbd77412af6172c
gabriel_grazziani <gabriel.grazzianibarbosa@gmail.com>
2021-05-17 13:43:41 -0300

    criando AutorRepository

---
+++ src/main/java/br/com/zupacademy/gabrielgarzziani/casadocodigo/autor/AutorRepository.java
@@ -0,0 +1,7 @@
+package br.com.zupacademy.gabrielgarzziani.casadocodigo.autor;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutorRepository extends JpaRepository<Autor, Long>{
+
+}
